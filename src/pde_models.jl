0ad73511d70f5aee8ada11443ae80cc5152d0a47
Jerry <jerryfung110@gmail.com>
2024-07-19 21:41:48 +0100

    fixed summation problem in pde_models

--- src/pde_models.jl
+++ src/pde_models.jl
@@ -123,16 +123,12 @@ end
 function _transition_state_model(problem, dom::PDEDiscretizationDomain; T = Float64, backend = nothing, kwargs...)
     a, b, c, d, p = problem.a, problem.b, problem.c, problem.d, problem.p
     x0 = _initial_position!(problem, dom, 10)
-    array = [
+    array1 = [
         (
             e1, 
             dom.TRIANG[e1, 1], 
             dom.TRIANG[e1, 2], 
             dom.TRIANG[e1, 3], 
-            b[dom.TRIANG[e1,1]], 
-            c[dom.TRIANG[e1,1]], 
-            d[dom.TRIANG[e1,1]], 
-            p[dom.TRIANG[e1,1]],
             dom.AREA[e1],
             dom.EDGE[e1, 1, 1],
             dom.EDGE[e1, 1, 2],
@@ -142,13 +138,25 @@ function _transition_state_model(problem, dom::PDEDiscretizationDomain; T = Floa
             dom.EDGE[e1, 3, 2]
         ) for e1 in 1:dom.ELEM
     ]
+    array2 = [
+    (
+        e1 = e1,
+        AREA = dom.AREA[e1],
+        b = b[dom.TRIANG[e1, c1]],
+        c = c[dom.TRIANG[e1, c1]],
+        d = d[dom.TRIANG[e1, c1]],
+        p = p[dom.TRIANG[e1, c1]],
+        TRIANG = dom.TRIANG[e1, c1]
+    )
+    for e1 in 1:dom.ELEM for c1 in 1:(dom.DIMEN + 1)
+]
     #_proto_model()
 
     ALPHA = 2.0
     H = ALPHA / (dom.BREAK+1) * sqrt(sum((dom.US[n] - dom.UE[n])^2 for n in 1:dom.NODES))
 
     # Build optimization problem
-    core = ExaModels.ExaCore(T; backend= backend)
+    core = ExaModels.ExaCore(T; backend = backend)
 
     u = ExaModels.variable(core, 1:dom.BREAK+2, 1:dom.NODES; start=x0.u)
     integral = ExaModels.variable(core, 1:dom.BREAK+2, 1:dom.ELEM)
@@ -182,11 +190,9 @@ function _transition_state_model(problem, dom::PDEDiscretizationDomain; T = Floa
         b1 => (u[b1+1, n] - u[b1, n])^2 for b1 in 1:dom.BREAK+1, n in 1:dom.NODES
     )
 
-    ExaModels.constraint(
+    c3 = ExaModels.constraint(
         core,
         AREA*(
-            1 / (dom.DIMEN+1) *
-                (((b*u[b1,TRIANG1]^2/2- c*u[b1,TRIANG1]^(p+1)/(p+1)+ d*u[b1, TRIANG1]))) +
             a / (8*AREA^2)*(
                 u[b1,TRIANG1]^2*(EDGE_21^2 + EDGE_22^2) +
                 u[b1,TRIANG2]^2*(EDGE_31^2 + EDGE_32^2) +
@@ -197,7 +203,15 @@ function _transition_state_model(problem, dom::PDEDiscretizationDomain; T = Floa
                 )
             ) 
             - integral[b1, e1]
-        for b1 in 1:dom.BREAK+2, (e1, TRIANG1, TRIANG2, TRIANG3, b, c, d, p, AREA, EDGE_11, EDGE_12, EDGE_21, EDGE_22, EDGE_31, EDGE_32) in array
+        for b1 in 1:dom.BREAK+2, (e1, TRIANG1, TRIANG2, TRIANG3, AREA, EDGE_11, EDGE_12, EDGE_21, EDGE_22, EDGE_31, EDGE_32) in array1
+    )
+
+    ExaModels.constraint!(
+        core,
+        c3,
+        (b1, e1) => AREA* 1 / (dom.DIMEN+1) *
+                    (b*u[b1,TRIANG]^2/2- c*u[b1,TRIANG]^(p+1)/(p+1)+ d*u[b1, TRIANG]) 
+                    for b1 in 1:dom.BREAK+2, (e1, AREA, b, c, d, p, TRIANG) in array2
     )
 
     # Boundary
@@ -258,4 +272,8 @@ function lane_emden_model(nh)
     return _transition_state_model(pb, dom)
 end
 
+using NLPModelsIpopt, ExaModels
 
+println("running test")
+model = dirichlet_model(20)
+ipopt(ExaModels.ExaModel(model))
