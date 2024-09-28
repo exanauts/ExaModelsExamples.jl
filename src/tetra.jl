e2fb7ca3370cd8cee2ef8b6bb30afb84c15d1880
jerryfung110 <jerryfung110@gmail.com>
2024-09-28 23:13:30 +0000

    modified formulation in tetra.jl

--- src/tetra.jl
+++ src/tetra.jl
@@ -23,6 +23,11 @@ function tetra_model(x0 = xe_tetra, TETS::Vector{Int64} = Tets_tetra, Const::Vec
     uvar[Const] .= x0[Const]
     uvar[Const .+ N] .= x0[Const .+ N]
     uvar[Const .+ 2 * N] .= x0[Const .+ 2 * N]
+    obj_array = [(TETS[e], TETS[e + E], TETS[e + 2*E], TETS[e + 3*E], TETS[e]+N, TETS[e]+2*N, TETS[e + E]+N, TETS[e + E]+2*N, TETS[e + 2*E]+N, TETS[e + 2*E]+2*N, TETS[e + 3*E]+N, TETS[e + 3*E]+2*N) for e in 1:E]
+    c1_array = [(e, TETS[e], TETS[e] + N * i, TETS[e] + N * mod(i + 1, 3), TETS[e] + N * mod(i - 1, 3), 
+                TETS[e + E] + N * i, 
+                TETS[e + 2E] + N * mod(i + 1, 3), TETS[e + 2E] + N * mod(i - 1, 3),
+                TETS[e + 3E] + N * mod(i + 1, 3), TETS[e + 3E] + N * mod(i - 1, 3)) for i in 0:2 for e in 1:E]
 
     c = ExaModels.ExaCore(T; backend = backend)
 
@@ -63,7 +68,7 @@ function tetra_model(x0 = xe_tetra, TETS::Vector{Int64} = Tets_tetra, Const::Vec
                 )) *
                 sqrt(2)
             )^(2 / 3) 
-        ) for (TETS, TETS_E, TETS_2E, TETS_3E, TETS_N, TETS_2N, TETS_E_N, TETS_E_2N, TETS_2E_N, TETS_2E_2N, TETS_3E_N, TETS_3E_2N) in [(TETS[e], TETS[e + E], TETS[e + 2*E], TETS[e + 3*E], TETS[e]+N, TETS[e]+2*N, TETS[e + E]+N, TETS[e+E]+2*N, TETS[e + 2*E]+N, TETS[e + 2*E]+2*N, TETS[e+ 3*E]+N, TETS[e + 3*E]+2*N) for e in 1:E]
+        ) for (TETS, TETS_E, TETS_2E, TETS_3E, TETS_N, TETS_2N, TETS_E_N, TETS_E_2N, TETS_2E_N, TETS_2E_2N, TETS_3E_N, TETS_3E_2N) in obj_array
     )
 
     c1 = ExaModels.constraint(
@@ -76,17 +81,17 @@ function tetra_model(x0 = xe_tetra, TETS::Vector{Int64} = Tets_tetra, Const::Vec
     ExaModels.constraint!(
         c,
         c1,
-        e =>    sum((x[TETS_E + N * i] - x[TETS + N * i]) *
+        e =>    (x[TETS_E_N] - x[TETS_N]) *
                 (
-                    (x[TETS_2E + N * mod(i + 1, 3)] - x[TETS + N * mod(i + 1, 3)]) *
-                    (x[TETS_3E + N * mod(i - 1, 3)] - x[TETS + N * mod(i - 1, 3)]) -
-                    (x[TETS_2E + N * mod(i - 1, 3)] - x[TETS + N * mod(i - 1, 3)]) *
-                    (x[TETS_3E + N * mod(i + 1, 3)] - x[TETS + N * mod(i + 1, 3)])
+                    (x[TETS_2E_plus] - x[TESTS_N_plus]) *
+                    (x[TETS_3E_minus] - x[TETS_N_minus]) -
+                    (x[TETS_2E_minus] - x[TETS_N_minus]) *
+                    (x[TETS_3E_plus] - x[TESTS_N_plus])
                 ) *
                 sqrt(2) 
-                for i in 0:2) for (e, TETS, TETS_E, TETS_2E, TETS_3E) in [(e, TETS[e], TETS[e + E], TETS[e + 2*E], TETS[e + 3*E]) for e in 1:E]
+                for (e, TETS, TETS_N, TESTS_N_plus, TETS_N_minus, TETS_E_N, TETS_2E_plus, TETS_2E_minus, TETS_3E_plus, TETS_3E_minus) in c1_array
     )
-
+    
     return ExaModels.ExaModel(c; kwargs...)
 end
 
